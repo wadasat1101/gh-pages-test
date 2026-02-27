ccf41058f7c96b95573240038c3d3dc207fa724d
saboxx1101 <sabo1985@gmail.com>
2026-02-28 08:55:30 +0900

    update

--- scripts/index.js
+++ scripts/index.js
@@ -445,11 +445,15 @@ document.querySelectorAll("button[data-interval]")
 	});
 
 Promise.all([
+	fetch("./symbols0.json").then(r => r.json()),
 	fetch("./symbols1.json").then(r => r.json()),
 	fetch("./symbols2.json").then(r => r.json()),
 	fetch("./symbols3.json").then(r => r.json()),
 	fetch("./symbols4.json").then(r => r.json()),
-	fetch("./symbols99.json").then(r => r.json()),
+	fetch("./symbols5.json").then(r => r.json()),
+	fetch("./symbols6.json").then(r => r.json()),
+	fetch("./symbols7.json").then(r => r.json()),
+	fetch("./symbols8.json").then(r => r.json()),
 ])
 .then((configs) => {
 	// configs は [cfg1, cfg2, cfg3] の配列
